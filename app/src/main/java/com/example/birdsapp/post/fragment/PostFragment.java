0358975ed38ebdb6dc7ba472f474509f83f872ee
amirulrushdan-bah <arushdan25@gmail.com>
2020-04-01 16:22:03 +0200

    update post fragment #5

--- app/src/main/java/com/example/birdsapp/post/fragment/PostFragment.java
+++ app/src/main/java/com/example/birdsapp/post/fragment/PostFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 import com.example.birdsapp.R;
@@ -17,11 +18,17 @@ public class PostFragment extends Fragment {
     public void onCreate(Bundle savedInstantState) {
         super.onCreate(savedInstantState);
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View rV= inflater.inflate(R.layout.activity_post,container,false);
         //TODO repmlir les cases de poste, nom auteur, date etc
-
+        final TextView nomAuteur = (TextView)rV.findViewById(R.id.nomAuteur);
+        final TextView nomEspece = (TextView)rV.findViewById(R.id.nomEspece);
+        final TextView lieu = (TextView)rV.findViewById(R.id.lieuObservation);
+        final TextView date = (TextView)rV.findViewById(R.id.dateDePublication);
+        final TextView description = (TextView)rV.findViewById(R.id.descriptionPost);
+        
         return rV;
     }
 }

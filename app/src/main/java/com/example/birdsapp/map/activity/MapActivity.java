089fb38daf1864e98dff1cfea0b73bc7dacdeb8f
ClementPoueyto <poueyto.clement@gmail.com>
2020-04-01 15:26:10 +0200

    ajout affichage carte #1

--- app/src/main/java/com/example/birdsapp/map/activity/MapActivity.java
+++ app/src/main/java/com/example/birdsapp/map/activity/MapActivity.java
@@ -1,6 +1,25 @@
 package com.example.birdsapp.map.activity;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.birdsapp.R;
+import com.example.birdsapp.map.fragment.MapFragment;
 
 public class MapActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        MapFragment mainFragment=(MapFragment) getSupportFragmentManager().findFragmentById(R.id.frame_map);
+        if(mainFragment==null){
+            FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_map,new MapFragment());
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+    }
 }
--- app/src/main/java/com/example/birdsapp/map/fragment/MapFragment.java
+++ app/src/main/java/com/example/birdsapp/map/fragment/MapFragment.java
@@ -1,6 +1,80 @@
 package com.example.birdsapp.map.fragment;
 
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
+
+import com.example.birdsapp.R;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
 
 public class MapFragment extends Fragment {
+    IMapController mapController;
+    private MapView map;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootView=inflater.inflate(R.layout.fragment_map,container,false);
+        this.map=initMap();
+        return rootView;
+    }
+
+    /**
+     * initialise la map
+     * @return map initialisée
+     */
+    private MapView initMap(){
+        MapView map;
+        Configuration.getInstance().load(getActivity().getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext()));
+        getActivity().setContentView(R.layout.fragment_map);
+        map= getActivity().findViewById(R.id.display_map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+
+        GeoPoint startPoint = new GeoPoint(43.1167,5.9333);
+        mapController = map.getController();
+        mapController.setZoom(18.0);
+        mapController.setCenter(startPoint);
+
+        ArrayList<OverlayItem> items = new ArrayList<>();
+        OverlayItem home = new OverlayItem(" Clement's home", "toulon",new GeoPoint(43.200,5.9333) );
+        Drawable m = home.getMarker(0);
+        items.add(home);
+        items.add(new OverlayItem("Resto", "le barathin", new GeoPoint(43.1170, 5.9335)));
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getActivity().getApplicationContext(),
+                items, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+            @Override
+            public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                return true;
+            }
+
+            @Override
+            public boolean onItemLongPress(int index, OverlayItem item) {
+                return false;
+            }
+        });
+        mOverlay.setFocusItemsOnTap(true);
+        map.getOverlays().add(mOverlay);
+
+        return map;
+    }
 }
